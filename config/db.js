d4cb7f975ff73bc7c87a29f78d39ce083074ac53
kiran-narapureddy <kirannarapureddy.cme@gmail.com>
2026-07-13 15:01:41 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
+const dns = require("dns")
+dns.setServers([8.8.8.8",8.8.4.4"])
 
 const connectDB = async () => {
   try {
     
-    await mongoose.connect('mongodb://localhost:27017/workshop_db');
+    await mongoose.connect('mongodb+srv://kirannarapureddy:#kiran@2007@cluster0.btibyn9.mongodb.net/kirannarapureddy');
     
     console.log('MongoDB connected successfully!');
   } catch (err) {
