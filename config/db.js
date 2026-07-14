fec9557f95d3089144db7d13e5c17acbc1b50bd8
kiran-narapureddy <kirannarapureddy.cme@gmail.com>
2026-07-14 10:03:15 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,12 +1,15 @@
 const mongoose = require('mongoose');
-const dns = require("dns")
-dns.setServers(["8.8.8.8","8.8.4.4"])
+
+const dns = require("dns");
+
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 const connectDB = async () => {
   try {
+    // MongoDB Atlas connection string
+    const connString = "mongodb+srv://kirannarapureddy:#kiran@2007@cluster0.btibyn9.mongodb.net/";
     
-    await mongoose.connect('mongodb+srv://kirannarapureddy:#kiran@2007@cluster0.btibyn9.mongodb.net/kirannarapureddy');
-    
+    await mongoose.connect(connString);
     console.log('MongoDB connected successfully!');
   } catch (err) {
     console.error('Database connection error:', err.message);
